17327bc990dd67e241fbe2bc620b4e64ea699844
Wahid Shaikh <wahidxzzz@gmail.com>
2021-02-17 00:11:08 +0530

    UI Almost Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./index.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World !</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleString(),
+    };
+  }
+  render() {
+    return (
+      <div className="app">
+        <div className="col">
+          <div className="card">
+            <div
+              className="aid"
+              unselectable="on"
+              onselectstart="return false;"
+              onmousedown="return false;"
+            >
+              <span>#12</span>
+            </div>
+            <div className="date">{this.state.date}</div>
+            <div id="adviceText">
+              It's not about who likes you, it's about who you like.
+            </div>
+            <div className="socialShare">
+              <div class="share">
+                <a
+                  href="//twitter.com/intent/tweet?text=${}"
+                  className="twitter"
+                  target="_blank"
+                >
+                  <i className="ri-twitter-line"></i>&nbsp;Make a TWEET
+                </a>
+              </div>
+              <div class="share">
+                <a
+                  href="//pinterest.com/pin/create/button/?url=%2F%2Fadviceslip.com%2F&amp;media=%2F%2Fapi.adviceslip.com%2Fadvice%2Fundefined%2Fimg%2Fm&amp;description=${}"
+                  className="pinterest"
+                  target="_blank"
+                >
+                  <i className="ri-pinterest-line"></i>&nbsp;PIN THIS
+                </a>
+              </div>
+              <div class="share">
+                <a
+                  href="//pinterest.com/pin/create/button/?url=%2F%2Fadviceslip.com%2F&amp;media=%2F%2Fapi.adviceslip.com%2Fadvice%2Fundefined%2Fimg%2Fm&amp;description=${}"
+                  className="pinterest"
+                  target="_blank"
+                >
+                  <i class="ri-facebook-fill"></i>&nbsp;FaceBook Share
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="searchBar">
+          <i class="ri-search-eye-line"></i>
+            <input type="text" name="searchIn" id="searchIn" placeholder="Search for Advice or Id Eg: 23,89"/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
